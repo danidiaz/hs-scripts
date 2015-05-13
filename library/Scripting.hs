d3d4a0f9fc414b92c527998bb245db147a017e11
Daniel Diaz <diaz.carrete@facebook.com>
2015-05-13 15:28:32 +0200

    New deps.

--- library/Scripting.hs
+++ library/Scripting.hs
@@ -1,2 +1,29 @@
-module Scripting () where 
+module Scripting (
+        module Scripting
+    ,   module Data.Monoid
+    ,   module Control.Applicative
+    ,   module Control.Monad
+    ,   module Control.Monad.Trans.Class
+    ,   module Control.Monad.Trans.Except
+    ,   module Control.Monad.Trans.Reader
+    ,   module Control.Monad.IO.Class
+    ,   module Control.Comonad
+    ,   module Control.Concurrent.Async
+    ,   module Control.Concurrent.Conceit
+    ,   module Control.Lens
+    ,   module Control.Lens.Wrapped
+    ) where 
 
+import Data.Monoid
+import Data.Bifunctor
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Except hiding (liftCallCC)
+import Control.Monad.Trans.Reader hiding (liftCallCC)
+import Control.Monad.IO.Class
+import Control.Comonad
+import Control.Concurrent.Async
+import Control.Concurrent.Conceit
+import Control.Lens
+import Control.Lens.Wrapped
